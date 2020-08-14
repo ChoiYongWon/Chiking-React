88722a1dc7868c4dc71f7759e538fb4ef15d847a
ChoiYongWon <yongwon0824@naver.com>
2020-08-14 16:43:12 +0900

    Renamed className based on Css Convention

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ require("../css/main.css")
 const Main = (props)=>{
     return (
         <>
-            <div className="background-wrap"></div>
+            <div className="background__wrap"></div>
         </>
     )
 }
